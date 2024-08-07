9f1f45d480bd10ba70c61d10118446a372e39682
Engineer of Sharettle <developer@sharettle.com>
2024-08-07 22:49:28 +0900

    Update 【駆け出しVSシニア】あなたは綺麗にかけるか？厳選10問.js

--- JavaScript/【駆け出しVSシニア】あなたは綺麗にかけるか？厳選10問.js
+++ JavaScript/【駆け出しVSシニア】あなたは綺麗にかけるか？厳選10問.js
@@ -2,4 +2,4 @@ const user1 = {name: "watanabe", age:27, isMen: true}
 const user2 = {name: "suzuki", age:25, isMen: false}
 
 console.log({user1, user2})
-console.table
+console.table([user1,user2])
