9f7ed798cc043f24429e43992f2a6c56bbb306b6
Engineer of Sharettle <developer@sharettle.com>
2024-08-09 18:52:54 +0900

    Update グローバルな情報管理.tsx

--- Next.js/Components/グローバルな情報管理.tsx
+++ Next.js/Components/グローバルな情報管理.tsx
@@ -1,3 +1,15 @@
+基本メソッド
+
+・createContext: コンテキストを作成
+・useContext: コンテキストから値を取得するためのフック
+・useEffect: 副作用を管理するフック。コンポーネントのライフサイクルに関連する処理を行う
+・useState:状態を管理するためのフック
+・ReactNode: コンポーネントの子要素の型
+・useEffect(() => {}, []);
+依存配列がからのときはマウント時に一度だけ実行される。
+
+
+
 ディレクトリ構造
 以下は、Next.jsのappディレクトリを使用して、グローバルなユーザー情報管理を行うためのディレクトリ構造です。
 /my-app
@@ -85,6 +97,8 @@ export default function RootLayout({children} : {children.React.Node}){
   );  
 }
 
+
+
 import {useUser} from './components/UserProvider';
 
 export default function HomePage(){
@@ -110,11 +124,86 @@ export default function HomePage(){
 
 // /app/profile/page.tsx
 
+import {useUser} from '../components/UserProvider';
+
+export default function ProfilePage(){
+const {user, loading} = useUser();
+
+if(Loding) return <div>Loading ...</div>
+  return(
+    <h1>Profile Page</h1>
+      {user ? (
+      <div>
+        <p>Name : {user.name)</p>
+        <p>Age : {user.age}</p>
+      </div>
+      ):(
+        <div>No user data available.</div>
+      )
+      
+  )
+};
+
+      
+import React. {createContext, useContext, useEffect, userState, ReactNode}
+import {auth, firestore} from '../../utils/firebase';
 
 
+interface UserContextType{
+ user : any;
+loading: boolean;      
+}
+
+  // userContextTypeというインターフェースを定義
+  // user , loading という2つのプロパティをもつ
+  // createContextでコンテキストを作成
+  //  アプリケーション全体でユーザー情報を共有できる
+
+const UserConText = createContext<UserContextType | undefined>(undefined);
+      
+export const User Provider = ({children}: {children: ReactNode}) => {
+// 役割: 子コンポーネントにユーザー情報とローディング状態を提供します。
+const [user, setUser] = useState<any>(null);
+const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() =>{ //ユーザー認証を監視しユーザー情報を取得して状態を更新する。
+    const unsubscribe  = auth.onAuthStateChanged(async (currentUser) => {
+      
+    
+  });
+
+  
+}
 
 
+コールバック関数:他の関数に引数として渡される関数。渡された関数は指定されたイベントや処理が完了したときに呼び出される
 
+  function callbackFunction(){
+  }
+  // コールバック関数
+
+  function performAction(callback){
+    callback()//コールバック関数を呼び出す
+  }
+
+
+  performActioin(callbackFunction);
+
+  function applyFunction(arr, callback){
+    for (let i = 0; i < arr.length; i++){
+      arr[i]  = callback(arr[i]);
+    }
+    return arr;
+  }
+
+  function double(x){
+    return x * 2;
+  }
+
+  const numbers  = [1,3,5,6]
+  const doubleNumbers = applyFunction(numbers, double);
+
+  console.log(doubleNumbers);
 
 
 
