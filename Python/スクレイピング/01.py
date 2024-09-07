5dec311d01f2b9ced97b51a7d93c2450b4b76b79
sharettleDeveloper <SharettleDeveloper@users.noreply.github.com>
2024-09-07 12:00:32 +0900

    Javascript

--- Python/スクレイピング/01.py
+++ Python/スクレイピング/01.py
@@ -1,6 +1,6 @@
-import requests
+# import requests
 
-response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-print(response.json())
-print(f"Title: {response.json()['title']}")
-print(f"Completed: {response.json()['completed']}")
+# response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+# print(response.json())
+# print(f"Title: {response.json()['title']}")
+# print(f"Completed: {response.json()['completed']}")
