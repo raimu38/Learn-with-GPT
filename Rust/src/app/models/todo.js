ecfa0bbd3f47f920bdae7ba21a58cb42cdfa52f3
raimu38 <176199024+raimu38@users.noreply.github.com>
2024-11-18 18:43:21 +0900

    internship

---
+++ Rust/src/app/models/todo.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const todoSheme = new mongoose.Scheme({
+	title:{type:String, required:true},
+	description:{type:String, required:true},
+	dueDate:{type:Date, required:true},
+	status:{type:String, enum:['まだやってない','継続中', '完了'], default:'まだやってない'}},{timestamp: true});
+
+
+module.exports = mongoose.model('Todo',todoSheme);
+
+
+
