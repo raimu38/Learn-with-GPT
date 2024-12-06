f7fe01444a21c5044d5e7922d8047e641869dd6e
sharettleDeveloper <SharettleDeveloper@users.noreply.github.com>
2024-12-06 23:51:22 +0900

    Object Class

---
+++ JavaScript/DokushuJavaScript/5seigyo/this.js
@@ -0,0 +1,9 @@
+const fruits = { apple: "りんご", banana: "バナナ" };
+const props = Object.keys(fruits);
+console.log(props);
+const values = Object.values(fruits);
+console.log(values);
+const entries = Object.entries(fruits);
+console.log(entries);
+console.log(fruits.values);
+console.
---
+++ Python/Fund/src/modules/main.py
@@ -0,0 +1,5 @@
+from modules import module1
+print(module1.greet("らいむ"))
+
+calc = module1.Calculator()
+print(f"5 + 3 = {calc.add(5, 3)}")
