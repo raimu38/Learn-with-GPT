7ce1f40f653ebd2ca8df3883dda44dfb3a58b743
sharettleDeveloper <SharettleDeveloper@users.noreply.github.com>
2024-12-09 23:45:34 +0900

    Django

--- JavaScript/DokushuJavaScript/5seigyo/this.js
+++ JavaScript/DokushuJavaScript/5seigyo/this.js
@@ -3,7 +3,18 @@ const props = Object.keys(fruits);
 console.log(props);
 const values = Object.values(fruits);
 console.log(values);
+
+for (prop of props) {
+  console.log(prop, fruits[prop]);
+}
+
+for (value of values) {
+  console.log(value);
+}
+
 const entries = Object.entries(fruits);
 console.log(entries);
-console.log(fruits.values);
-console.
+
+for (entry of entries) {
+  console.log(entry[0], entry[1]);
+}
