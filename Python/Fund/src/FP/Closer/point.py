156e3c41dd39d7516ef47de9c655ebc2090e5352
sharettleDeveloper <SharettleDeveloper@users.noreply.github.com>
2024-12-14 00:41:33 +0900

    closer point

---
+++ Python/Fund/src/FP/Closer/point.py
@@ -0,0 +1,10 @@
+def point_calculator(rate):
+    def calculate_point(amount):
+        return amount*(1-rate)
+    return calculate_point
+
+normal_point = point_calculator(0.01)
+campain_point = point_calculator(0.05)
+
+print(normal_point(1000))
+print(campain_point(1000))
---
+++ Python/Fund/src/FP/Closer/point2.py
@@ -0,0 +1,15 @@
+def create_conditional_calurator(base_rate, threshold=None, bonus_rate=None):
+    def calculate_point(amount):
+        point = amount * base_rate
+        if threshold and amount > threshold:
+            point += amount*bonus_rate
+        return point
+    return calculate_point
+
+base_calcurator = create_conditional_calurator(0.01)
+bonus_calcurator = create_conditional_calurator(0.01,threshold=10000, bonus_rate=0.1)
+
+print(base_calcurator(5000))
+print(base_calcurator(12000))
+print(bonus_calcurator(5000))
+print(bonus_calcurator(12000))
