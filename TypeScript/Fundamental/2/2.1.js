3073e25bd06456e2f45f88780fa1b4176a58d6ae
sharettleDeveloper <SharettleDeveloper@users.noreply.github.com>
2024-12-19 15:56:00 +0900

    typescript kata array

--- TypeScript/Fundamental/2/2.1.js
+++ TypeScript/Fundamental/2/2.1.js
@@ -1,4 +1,14 @@
-function sayhello(hello, age) {
-    console.log(hello + "あなたは" + age + "歳です");
+function kata() {
+    var age = 30;
+    var name = "katou";
+    var isDone = true;
+    var array = [];
+    var mixedArrayT = [["foo", 1]];
+    array.push("konnnitiha");
+    array.push(1);
+    mixedArrayT.push(["woo", 9]);
+    console.log(mixedArrayT);
+    console.log(mixedArrayT[1][1]);
+    console.log(mixedArrayT.length);
 }
-sayhello("こんにちわ", 34);
+kata();
