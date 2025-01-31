d9f74594c1133c7b26f4bc70296a51a55ce74a94
raimu38 <176199024+raimu38@users.noreply.github.com>
2025-01-31 23:59:21 +0900

    react useRef

---
+++ HTML.CSS/Tailwind/learn-tailwind/src/pages/canva/index.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
+
+const MyChart = () => {
+  const chartRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    if (!chartRef.current) return;
+
+    const ctx = chartRef.current.getContext("2d");
+    if (!ctx) return;
+
+    const myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 3, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+    return () => {
+      myChart.destroy();
+    };
+  }, []);
+
+  return (
+    <div>
+      <canvas ref={chartRef}></canvas>
+    </div>
+  );
+};
+
+export default MyChart;
