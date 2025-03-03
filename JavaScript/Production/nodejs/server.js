a5234df6a703e144a381bacc644f0e34b601e326
sharettleDeveloper <SharettleDeveloper@users.noreply.github.com>
2025-03-03 23:49:09 +0900

    nodejs Tutoaril

--- JavaScript/Production/nodejs/server.js
+++ JavaScript/Production/nodejs/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get("/", (req, res) => {
-  res.send("Hello NodeJS");
+  res.send("HEllo Node JS");
 });
 
 app.listen(port, () => {
-  console.log("App Listening on port ${port} ");
+  console.log(`Sever Sucsess!! port:${port}`);
+});
+
+app.post("/user", (req, res) => {
+  res.send("Got a PUT at /user");
+});
+
+app.put("/user", (res, req) => {
+  res.send("Gout a PUT at /user");
+});
+
+app.delete("/user", (res, req) => {
+  res.send("Got a DElete at /user");
 });
