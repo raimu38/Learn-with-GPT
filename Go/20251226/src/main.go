520f85919445a9f74dd4348fb82ba96ed0bd2175
raimu38 <176199024+raimu38@users.noreply.github.com>
2025-12-27 17:52:59 +0900

    go hello world

---
+++ Go/20251226/src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println("Hello, World")
+
+	var nameOne string = "RAIMU"
+	nameOne = "DANGOMUSI"
+	var nameTwo = "BANANA"
+	var nameThree string = "sss"
+	nameThree = "CAROT"
+	nameTwo += nameThree + nameThree
+
+	fmt.Println(nameOne)
+	fmt.Println((nameTwo))
+	
+
+}
