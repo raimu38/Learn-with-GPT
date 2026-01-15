3a7323b1875b6a413e4881f4b178359206813069
raimu38 <176199024+raimu38@users.noreply.github.com>
2026-01-15 23:58:32 +0900

    C言語をもう一度

---
+++ C/9cguide.appspot.com/01-01.c
@@ -0,0 +1,18 @@
+//型 関数名(引数) {処理}
+int hello(void)
+{
+    return 0;
+} 
+int helo(void);
+int main(void)
+{
+  /*hello()を4回だけにしてください*/
+    hello();
+    hello();
+    hello();
+    helo();
+} 
+
+int helo(void){return 0;}
+//Cはmainから始まる　これ以外の関数だけを書いても何も始まらない
+
