727fae4c3c615188664458de50d291d5ccac7ca7
raimu38 <176199024+raimu38@users.noreply.github.com>
2026-01-16 19:34:13 +0900

    rでビルド実行を自動化

--- C/9cguide.appspot.com/helloworld.c
+++ C/9cguide.appspot.com/helloworld.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main(void){
-  printf("Hello world");
+  printf("Hello\tworld\n");
+  printf("Hellogyouza\tkonishisan\n");
+  printf("Konnitiha\twatasiha \tarigatou woiemasuyo\n");
+  printf("%d円\n%f%%割引\n",100,33.0);
   return 1;
 }
